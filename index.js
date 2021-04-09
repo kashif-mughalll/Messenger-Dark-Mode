47ae36dbb02c215102725d29aef3ee72a09efd11
kashif mughal <123m.kashifmughal@gmail.com>
2021-04-09 18:17:50 +0500

    done

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-console.log("food")
+var InputBarWidth = 17;
 
-console.log(document.querySelectorAll("i"))
+function InputBarAnimation(){
+    document.getElementById("Hid1").style.display = "none";
+    document.getElementById("Hid2").style.display = "none";
+    document.getElementById("Hid3").style.display = "none";
+    document.getElementById("messagesContainerTray").id = "messagesContainerTray2";
+    document.getElementById("InputBar").style.width = "25rem";
+
+    var IntervalId = setInterval(() => {
+        if(InputBarWidth < 25){
+            InputBarWidth += 0.25;
+            document.getElementById("InputBar").style.width = InputBarWidth + "rem";
+        }else{
+            clearInterval(IntervalId);
+        }
+    }, 8);
+}
+
+document.getElementById("InputBar").addEventListener('click',()=> {
+    InputBarAnimation();
+})
+
+
